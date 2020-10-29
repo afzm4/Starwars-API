c8e44e22372c78852f365a63a88fdea96be2b1df
Andrew Floyd <afzm4@mst.edu>
2020-10-29 09:29:10 -0500

    Updated welcome message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const https = require('https')
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Test Test')
+  res.send('Hello, welcome to my Swapi.dev API')
 });
 
 var favorites = [];
